d61a3e98a2eb410ebec5154b978c54531db6a61b
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-02-28 12:32:42 +0300

    EF config

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(200);
+
+            builder.Property(x => x.Price).HasPrecision(18, 2);
+        }
+    }
+}
---
+++ src/Infrastructure/Data/WatchWorldContext.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class WatchWorldContext : DbContext
+    {
+        public WatchWorldContext(DbContextOptions<WatchWorldContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
