5d8230ac801a1d6f34728367db6d1be00ad8f14e
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-03-12 10:06:03 +0300

    Application Order

---
+++ src/ApplicationCore/Services/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBasketService _basketService;
+        private readonly IRepository<Order> _orderRepo;
+
+        public OrderService(IBasketService basketService, IRepository<Order> orderRepo)
+        {
+            _basketService = basketService;
+            _orderRepo = orderRepo;
+        }
+
+        public async Task<Order> CreateOrderAsync(string buyerId, Address shippingAddress)
+        {
+            var basket = await _basketService.GetOrCreateBasketAsync(buyerId);
+
+            // todo: must throw exception if basket is empty
+
+            Order order = new Order()
+            {
+                ShipToAddress = shippingAddress,
+                BuyerId = buyerId,
+                Items = basket.Items.Select(x => new OrderItem()
+                {
+                    ProductId = x.ProductId,
+                    Quantity = x.Quantity,
+                    ProductName = x.Product.Name,
+                    UnitPrice = x.Product.Price,
+                    PictureUri = x.Product.PictureUri
+                }).ToList()
+            };
+
+            return await _orderRepo.AddAsync(order);
+        }
+    }
+}
